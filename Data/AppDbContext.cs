e4391a1f45f68d073973d5c78009e39063f2c531
marcioygor <marcioygorsilva@gmail.com>
2022-04-02 12:54:06 -0300

    new commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Minimal_API.Models;
 
+
 namespace Minimal_API.Data
 {
     public class AppDbContext : DbContext
--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using System;
 using Minimal_API.ViewModels;
 using Minimal_API.Data;
+using Minimal_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>();
@@ -11,6 +15,19 @@ app.MapGet("v1/todos", (AppDbContext context) =>
     var todo = context.Todos.ToList();
     return Results.Ok(todo);
 });
+
+
+app.MapGet("v1/todos/{id}", (AppDbContext context, Guid id) =>
+{
+    var todo = context.Todos
+    .AsNoTracking()
+    .FirstOrDefault(x => x.id == id);
+
+    if(todo==null) return Results.NotFound("Registro não encontrado.");
+
+    return Results.Ok(todo);
+});
+
 
 app.MapPost("v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
 {
@@ -25,5 +42,39 @@ app.MapPost("v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
     return Results.Created($"/v1/todos/{todo.id}", todo);
 
 });
+
+app.MapPut("v1/todos/{id}", async (AppDbContext context, Guid id, Todo todo) =>
+{
+    var todoBanco = await context.Todos
+                .AsNoTracking<Todo>()
+                .FirstOrDefaultAsync(x => x.id == id);
+
+    if (todoBanco==null) return Results.NotFound();
+
+    context.Todos.Update(todo);
+
+    var result=context.SaveChanges();
+ 
+    return result >0 ? Results.NoContent(): Results.BadRequest("Houve um problema ao atualizar o registro.");
+
+});
+
+
+app.MapDelete("v1/todos/{id}", async ([FromServices] AppDbContext context, Guid id) =>
+{
+    var todo = await context.Todos
+                .AsNoTracking<Todo>()
+                .FirstOrDefaultAsync(x => x.id == id);
+
+    if (todo==null) return Results.NotFound("Registro não encontrado.");
+
+    context.Todos.Remove(todo);
+
+    var result=context.SaveChanges();
+ 
+    return result >0 ? Results.NoContent(): Results.BadRequest("Houve um problema ao apagar o registro.");
+
+});
+
 
 app.Run();
